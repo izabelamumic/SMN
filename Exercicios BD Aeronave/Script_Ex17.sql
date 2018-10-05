d6502d643224b2e7e8047562c0c96dba6124fa78
Izabela Mumic <iza95mumic@gmail.com>
2018-10-05 14:17:46 -0300

    Atualização exercicios

--- Exercicios BD Aeronave/Script_Ex17.sql
+++ Exercicios BD Aeronave/Script_Ex17.sql
@@ -1,226 +1,129 @@
 /*
 	17 - Levantar Média de Ocorrências por Mês e Ano
 */
+CREATE TABLE #tbTemp(
+	Ano INT, 
+	Janeiro INT, Fevereiro INT, Março INT, Abril INT, Maio INT, Junho INT,
+	Julho INT, Agosto INT, Setembro INT, Outubro INT, Novembro INT, Dezembro INT, 
+	TotalAno INT, MediaAno Decimal(10,2)
+)
 
-declare @media2008 int, @media2009 int,  @media2010 int, @media2011 int, @media2012 int,
-		@media2013 int, 	@media2014 int, @media2015 int, @media2016 int, @media2017 int, @media2018 int
+INSERT INTO #tbTemp(Ano, Janeiro, Fevereiro, Março, Abril, Maio, Junho,
+					Julho, Agosto, Setembro, Outubro, Novembro, Dezembro, 
+					TotalAno, MediaAno)
 
-SELECT	@media2008 = AVG(total.Media2008),
-		@media2009 = AVG(total.Media2009), 
-		@media2010 = AVG(total.Media2010),
-		@media2011 = AVG(total.Media2011),	  
-		@media2012 = AVG(total.Media2012),
-		@media2013 = AVG(total.Media2013),
-		@media2014 = AVG(total.Media2014),
-		@media2015 = AVG(total.Media2015),
-		@media2016 = AVG(total.Media2016),
-		@media2017 = AVG(total.Media2017),
-		@media2018 = AVG(total.Media2018)
+		(SELECT	Ano, 
+			TotalOco.Janeiro, TotalOco.Fevereiro, TotalOco.Março, TotalOco.Abril, TotalOco.Maio, TotalOco.Junho, TotalOco.Junho,
+			TotalOco.Agosto, TotalOco.Setembro, 	TotalOco.Outubro, TotalOco.Novembro, TotalOco.Dezembro, TotalOco.TotalAno, 
+			CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),(TotalOco.TotalAno)) / 12))  AS MediaAno
 
-	FROM ( SELECT YEAR(CONVERT(DATETIME, oco.ocorrencia_dia)) as Ano,		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 01
-						THEN 1
-				END 
-				) AS Janeiro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 02
-						THEN 1
-				END 
-				) AS Fevereiro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 03
-						THEN 1
-				END 
-				)  AS Março,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 04
-						THEN 1
-				END 
-				)  AS Abril,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 05
-						THEN 1
-				END 
-				) AS Maio,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 06
-						THEN 1
-				END 
-				)  AS Junho,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 07
-						THEN 1
-				END 
-				) AS Julho,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 08
-						THEN 1
-				END 
-				) AS Agosto,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 09
-						THEN 1
-				END 
-				) AS Setembro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 10
-						THEN 1
-				END 
-				)  AS Outubro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 11
-						THEN 1
-				END 
-				)  AS Novembro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 12
-						THEN 1
-				END 
-				)  AS Dezembro,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) BETWEEN 01 AND 12 
-						THEN 1
-				END 
-				) AS TotalAno,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2008
-						THEN 1
-				END 
-				) AS Media2008,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2009
-						THEN 1
-				END 
-				) AS Media2009,
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2010
-						THEN 1
-				END 
-				) AS Media2010,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2011
-						THEN 1
-				END 
-				) AS Media2011,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2012
-						THEN 1
-				END 
-				) AS Media2012,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2013
-						THEN 1
-				END 
-				) AS Media2013,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2014
-						THEN 1
-				END 
-				) AS Media2014,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2015
-						THEN 1
-				END 
-				) AS Media2015,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2016
-						THEN 1
-				END 
-				) AS Media2016,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2017
-						THEN 1
-				END 
-				) AS Media2017,
-		
-		COUNT(
-				CASE 
-					WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 2018
-						THEN 1
-				END 
-				) AS Media2018
-						
-				FROM oco WITH(NOLOCK)
+		FROM (SELECT YEAR(CONVERT(DATETIME, oco.ocorrencia_dia)) as Ano,		
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 01
+							THEN 1
+					END 
+					) AS Janeiro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 02
+							THEN 1
+					END 
+					) AS Fevereiro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 03
+							THEN 1
+					END 
+					)  AS Março,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 04
+							THEN 1
+					END 
+					)  AS Abril,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 05
+							THEN 1
+					END 
+					) AS Maio,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 06
+							THEN 1
+					END 
+					)  AS Junho,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 07
+							THEN 1
+					END 
+					) AS Julho,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 08
+							THEN 1
+					END 
+					) AS Agosto,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 09
+							THEN 1
+					END 
+					) AS Setembro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 10
+							THEN 1
+					END 
+					)  AS Outubro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = @qtdLinha
+							THEN 1
+					END 
+					)  AS Novembro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) = 12
+							THEN 1
+					END 
+					)  AS Dezembro,
+			COUNT(
+					CASE 
+						WHEN MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) BETWEEN 01 AND 12 
+							THEN 1
+					END 
+					) AS TotalAno
+	
+				FROM 	oco WITH(NOLOCK)
 				GROUP BY YEAR(CONVERT(DATETIME, oco.ocorrencia_dia))
-				--ORDER BY Ano
-			) AS total
+			)TotalOco
+		)
 
-	SELECT YEAR(CONVERT(DATETIME, oco.ocorrencia_dia)) AS Ano, 
-			@media2008 AS Media2008,
-			@media2009 AS Media2009,
-			@media2010 AS Media2010,
-			@media2011 AS Media2011,
-			@media2012 AS Media2012,
-			@media2013 AS Media2013,
-			@media2014 AS Media2014,
-			@media2015 AS Media2015,
-			@media2016 AS Media2016,
-			@media2017 AS Media2017,
-			@media2018 AS Media2018
-	FROM oco WITH(NOLOCK)
-	GROUP BY YEAR(CONVERT(DATETIME, oco.ocorrencia_dia))
-	order by Ano
+-- executa tudo junto a partir daqui
 
--- REFAZER
---SELECT  AVG(TotalMes.QuantidadeMes) as MediaMes,
---		AVG(TotalAno.QuantidadeAno) as MediaAno
---	FROM (SELECT
---				MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) as Mes,
---				COUNT(oco.ocorrencia_dia) as QuantidadeMes
---			FROM oco WITH(NOLOCK)
---			GROUP BY MONTH(CONVERT(DATETIME, oco.ocorrencia_dia))
---		) as TotalMes,
+declare @qtdLinha int
+set @qtdLinha = (select count(*) from #tbTemp)
 
---		(SELECT
---				YEAR(CONVERT(DATETIME, oco.ocorrencia_dia)) as Ano,
---				COUNT(oco.ocorrencia_dia) as QuantidadeAno
---			FROM oco WITH(NOLOCK)
---			GROUP BY YEAR(CONVERT(DATETIME, oco.ocorrencia_dia))
---		) as TotalAno
+SELECT * FROM #tbTemp
+UNION
+SELECT	NULL AS Ano,
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2), temp.Janeiro) / @qtdLinha))), 
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Fevereiro) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Março) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Abril) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Maio) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Junho) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Julho) / @qtdLinha))), 
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Agosto) / @qtdLinha))),
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Setembro) / @qtdLinha))), 
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Outubro) / @qtdLinha))), 
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Novembro) / @qtdLinha))), 
+		SUM(CONVERT(DECIMAL(10,2),(CONVERT(DECIMAL(10,2),temp.Dezembro) / @qtdLinha))),
+		NULL AS TotalAno, NULL AS TotalMedia
 
--- mes
---SELECT	MONTH(CONVERT(DATETIME, oco.ocorrencia_dia)) as Mes,
---		COUNT(oco.ocorrencia_dia) as QuantidadeMes
---	FROM oco WITH(NOLOCK)
---	GROUP BY MONTH(CONVERT(DATETIME, oco.ocorrencia_dia))
---	ORDER BY Mes
+	FROM #tbTemp as temp
+order by Ano DESC
 
--- ano
---SELECT	YEAR(CONVERT(DATETIME, oco.ocorrencia_dia)) as Ano,
---		COUNT(oco.ocorrencia_dia) as QuantidadeAno
---	FROM oco WITH(NOLOCK)
---	GROUP BY YEAR(CONVERT(DATETIME, oco.ocorrencia_dia))
---	ORDER BY Ano
-
---	select oco.ocorrencia_dia from oco
--- Exercicios BD Aeronave/Script_Ex18.sql
+++ Exercicios BD Aeronave/Script_Ex18.sql
@@ -1,14 +1,12 @@
 /*
 	18 - Levantar Média de Fatalidades por Hora (sem minutos)
 */
--- REFAZER
-SELECT  AVG(Resultado.TotalFatalidade) as MediaPorHora
-	FROM (SELECT
-				DATEPART(HH, oco.ocorrencia_horario) as Hora ,
-				COUNT(DATEPART(HH, oco.ocorrencia_horario)) as TotalFatalidade
-			FROM  oco WITH(NOLOCK) 
-			GROUP BY DATEPART(HH, oco.ocorrencia_horario) 
-		) as Resultado
 
-		0411133905300
-		0411143730300
+SELECT	DATEPART(HOUR, oco.ocorrencia_horario) as Hora,
+		SUM(aer.total_fatalidades) as Fatalidade,
+		CONVERT(DECIMAL(10,4),(CONVERT(DECIMAL(10,4),(SUM(aer.total_fatalidades))) / (DATEDIFF(DAY,(MIN(oco.ocorrencia_dia)),MAX(oco.ocorrencia_dia)))))  AS MediaHora
+	FROM oco WITH(NOLOCK)
+	INNER JOIN  anv aer	WITH(NOLOCK)
+		ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
+	GROUP BY DATEPART(HOUR, oco.ocorrencia_horario)
+	order by HORA asc 
