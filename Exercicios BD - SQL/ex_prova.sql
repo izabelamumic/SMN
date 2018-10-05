cc869a9be08844b35dd023ad6d0a644971a63002
Izabela Mumic <iza95mumic@gmail.com>
2018-10-05 13:35:09 -0300

    Exercicios 19

---
+++ Exercicios BD - SQL/ex_prova.sql
@@ -0,0 +1,58 @@
+SELECT	cliente.IdCliente,
+		cliente.NomeCliente,
+		pri.PrimeiraDataVenda,
+		pri.QuantidadeItem,
+		pri.ValorTotal,
+		ul.UltimaDataVenda,
+		ul.QuantidadeItem,
+		ul.ValorTotal
+	FROM (SELECT		cl.IdCliente,
+					cl.NomeCliente
+			FROM Cliente cl WITH(NOLOCK)
+			)cliente
+		
+
+CROSS APPLY( SELECT	TOP(1)
+					MIN(ve.DataVenda) AS PrimeiraDataVenda,
+					vi.Quantidade as QuantidadeItem,
+					(vi.Quantidade * pr.ValorVenda) as ValorTotal
+				FROM Venda ve WITH(NOLOCK)
+				inner join Cliente cl
+					on ve.IdCliente = cl.IdCliente
+				INNER JOIN VendaItem vi WITH(NOLOCK)
+					ON ve.IdVenda = vi.IdVenda
+				INNER JOIN Produto pr WITH(NOLOCK)
+					ON vi.IdProduto = pr.IdProduto
+				
+			WHERE ve.IdCliente = cliente.IdCliente
+				GROUP BY vi.Quantidade, vi.Quantidade, pr.ValorVenda
+				order by PrimeiraDataVenda
+				
+			)pri
+
+CROSS APPLY( SELECT	TOP(1)
+				MAX(ve.DataVenda) AS UltimaDataVenda,
+					vi.Quantidade as QuantidadeItem,
+					(vi.Quantidade * pr.ValorVenda) as ValorTotal
+				FROM Venda ve WITH(NOLOCK)
+				inner join Cliente cl
+					on ve.IdCliente = cl.IdCliente
+				INNER JOIN VendaItem vi WITH(NOLOCK)
+					ON ve.IdVenda = vi.IdVenda
+				INNER JOIN Produto pr WITH(NOLOCK)
+					ON vi.IdProduto = pr.IdProduto
+				
+			WHERE ve.IdCliente = cliente.IdCliente
+				GROUP BY vi.Quantidade, vi.Quantidade, pr.ValorVenda
+				order by UltimaDataVenda DESC
+			)ul
+
+	group by		cliente.IdCliente,
+				cliente.NomeCliente,
+				pri.PrimeiraDataVenda,
+				pri.QuantidadeItem,
+				pri.ValorTotal,
+				ul.UltimaDataVenda,
+				ul.QuantidadeItem,
+				ul.ValorTotal
+	order by cliente.IdCliente
--- Exercicios BD Aeronave/Script_Ex19.sql
+++ Exercicios BD Aeronave/Script_Ex19.sql
@@ -1,64 +1,42 @@
-/*
-	19 - Levantar Fabricantes e a Primeira e Última Ocorrência.
-	Deve Retornar as seguintes informações para a Primeira e Última Ocorrência:
-	Tipo de Veículo, Número de Fatalidades e Nível de Dano
-*/
-SELECT	PrimeiraOcorrencia.ocorrencia_dia
-		aeronave_fabricante,
-		UltimaOcorrencia.aeronave_fabricante,
-		aer.aeronave_tipo_veiculo,
-		aer.total_fatalidades,
-		aer.aeronave_nivel_dano
-	FROM anv aer WITH(NOLOCK)
-	OUTER APPLY (
-		SELECT	MIN(oco.ocorrencia_dia) AS PrimeiraOcorrencia
-			FROM oco WITH(NOLOCK)
-			GROUP BY MIN(oco.ocorrencia_dia)
-			ORDER BY PrimeiraOcorrencia
-			) PrimeiraOcorrencia
-	OUTER APPLY (
-		SELECT	MAX(oco.ocorrencia_dia) AS UltimaOcorrencia
-			FROM oco WITH(NOLOCK)
-			GROUP BY MAX(oco.ocorrencia_dia)
-			ORDER BY UltimaOcorrencia
-			) UltimaOcorrencia
-	GROUP BY	 aer.aeronave_fabricante,aer.aeronave_tipo_veiculo, aer.total_fatalidades, aer.aeronave_nivel_dano
-	
+SELECT	maxmin.aeronave_fabricante,
+		maxmin.PrimeiraDiaOcorrencia,
+		primeiro.aeronave_tipo_veiculo,
+		primeiro.total_fatalidades,
+		primeiro.aeronave_nivel_dano,
+		maxmin.ultimoDiaOcorrencia,
+		ultimo.aeronave_tipo_veiculo,
+		ultimo.total_fatalidades,
+		ultimo.aeronave_nivel_dano
+	FROM (SELECT aer.aeronave_fabricante,
+				MIN(oco.ocorrencia_dia) AS PrimeiraDiaOcorrencia,
+				MIN(oco.ocorrencia_horario) AS PrimeiraHoraOcorrencia,
+				MAX(oco.ocorrencia_dia) AS ultimoDiaOcorrencia,
+				MAX(oco.ocorrencia_horario) AS ultimoHoraOcorrencia
+			FROM anv aer
+			INNER JOIN oco WITH(NOLOCK)
+				ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
+			group by aer.aeronave_fabricante
+		)maxmin
+CROSS APPLY(
+		SELECT	aer.aeronave_tipo_veiculo,
+				aer.total_fatalidades,
+				aer.aeronave_nivel_dano
+			FROM  anv aer WITH(NOLOCK)
+			INNER JOIN oco WITH(NOLOCK)
+				ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
+			WHERE oco.ocorrencia_dia = maxmin.PrimeiraDiaOcorrencia AND maxmin.aeronave_fabricante = aer.aeronave_fabricante
+	)primeiro
+CROSS APPLY(
+		SELECT	aer.aeronave_tipo_veiculo,
+				aer.total_fatalidades,
+				aer.aeronave_nivel_dano
+			FROM  anv aer WITH(NOLOCK)
+			INNER JOIN oco WITH(NOLOCK)
+				ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
+			WHERE oco.ocorrencia_dia = maxmin.ultimoDiaOcorrencia AND maxmin.aeronave_fabricante = aer.aeronave_fabricante
+	)ultimo
 
-
-SELECT	aer.aeronave_fabricante, 
-		MIN(oco.ocorrencia_dia) as PrimeiraOcorrencia,
-		MAX(oco.ocorrencia_dia) as UltimaOcorrencia
-	FROM oco WITH(NOLOCK)
-	INNER JOIN anv aer WITH(NOLOCK)
-		ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
-	GROUP BY	 aer.aeronave_fabricante
-	ORDER BY aer.aeronave_fabricante
-
-select oco.ocorrencia_dia, aer.aeronave_fabricante
-	from  oco
-	inner join anv aer
-		on oco.codigo_ocorrencia = aer.codigo_ocorrencia
-	order by aer.aeronave_fabricante
-
-	----
-
-
-SELECT	MIN(oco.ocorrencia_dia) as PrimeiraOcorrencia,
-		aer.aeronave_fabricante, 
-		aer.aeronave_tipo_veiculo, aer.total_fatalidades, aer.aeronave_nivel_dano,
-		MAX(oco.ocorrencia_dia) as UltimaOcorrencia,
-		aer.aeronave_fabricante, 
-		aer.aeronave_tipo_veiculo, aer.total_fatalidades, aer.aeronave_nivel_dano
-	FROM oco WITH(NOLOCK)
-	INNER JOIN anv aer WITH(NOLOCK)
-		ON oco.codigo_ocorrencia = aer.codigo_ocorrencia
-	GROUP BY	 aer.aeronave_fabricante, aer.aeronave_tipo_veiculo, aer.total_fatalidades, aer.aeronave_nivel_dano
-	ORDER BY aer.aeronave_fabricante
-
-select oco.ocorrencia_dia, aer.aeronave_fabricante, aer.aeronave_tipo_veiculo, aer.total_fatalidades, aer.aeronave_nivel_dano
-	from  oco
-	inner join anv aer
-		on oco.codigo_ocorrencia = aer.codigo_ocorrencia
-	order by aer.aeronave_fabricante
-
+	GROUP BY maxmin.aeronave_fabricante,maxmin.PrimeiraDiaOcorrencia,maxmin.PrimeiraDiaOcorrencia,
+			primeiro.aeronave_tipo_veiculo, 	primeiro.total_fatalidades, 	primeiro.aeronave_nivel_dano,
+			maxmin.ultimoDiaOcorrencia, 	ultimo.aeronave_tipo_veiculo, ultimo.total_fatalidades, 	ultimo.aeronave_nivel_dano
+	ORDER BY maxmin.aeronave_fabricante
