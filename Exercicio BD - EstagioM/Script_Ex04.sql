d69861cf44c4b4c38289da4dbaca3ee33b0fa2d7
Izabela Mumic <iza95mumic@gmail.com>
2018-10-15 15:59:09 -0300

    Atualização

--- Exercicio BD - EstagioM/Script_Ex04.sql
+++ Exercicio BD - EstagioM/Script_Ex04.sql
@@ -2,12 +2,16 @@
 	[10/10/2018] Eduardo: Retornar todos os clientes ativos que compraram lotes a partir de 2008
 	Caso o cliente possuía outro lote ativo antes da compra em 2008, retornar os lotes em outra coluna.
 */
-SELECT	(CONVERT(VARCHAR(50), lt.Num_Empr) + ' - ' + lt.Cod_Quadra + ' - ' + CONVERT(VARCHAR(50), lt.Cod_Lot)) AS Eql,
+
+SELECT	ROW_NUMBER() OVER(PARTITION BY cl.Num_CliCPFCNPJ ORDER BY cl.Num_CliCPFCNPJ) AS numRow,
+		(CONVERT(VARCHAR(50), lt.Num_Empr) + '-' + lt.Cod_Quadra + '-' + CONVERT(VARCHAR(50), lt.Cod_Lot)) AS Eql,
 		cl.Num_CliCPFCNPJ,
 		cl.Nom_Cli,
 		ct.Dat_Venda,
 		ct.Dat_ConfirmaVenda,
-		sv.Nom_StatusContVend
+		sv.Nom_StatusContVend,
+		lt.Cod_Lot
+	INTO #tbMais2008
 	FROM GKSLT_Contratos ct WITH(NOLOCK)
 	INNER JOIN GKSLT_Clientes cl WITH(NOLOCK)
 		ON ct.Num_CliCPFCNPJ = cl.Num_CliCPFCNPJ
@@ -17,8 +21,69 @@ SELECT	(CONVERT(VARCHAR(50), lt.Num_Empr) + ' - ' + lt.Cod_Quadra + ' - ' + CONV
 		ON ct.Cod_StatusContVend = sv.Cod_StatusContVend
 	WHERE	YEAR(ct.Dat_Venda) >= 2008
 			AND sv.Cod_StatusContVend = 2
+	ORDER BY ct.Dat_Venda DESC
 
-select ct.Cod_StatusContVend, sv.Nom_StatusContVend from GKSLT_Contratos ct
-inner join GKSLT_StatusContVend  sv
-on ct.Cod_StatusContVend = sv.Cod_StatusContVend
-group by ct.Cod_StatusContVend, sv.Nom_StatusContVend
+
+SELECT	SUBSTRING(mostra,1,LEN(mostra) - 1) AS EqlJunto,
+		tbM.Num_CliCPFCNPJ,
+		tbM.Nom_Cli,
+		tbM.Dat_Venda,
+		tbM.Dat_ConfirmaVenda,
+		tbM.Nom_StatusContVend
+	INTO #tbTempJunta2008
+	FROM #tbMais2008 tbM WITH(NOLOCK)
+	CROSS APPLY(SELECT (CONVERT(VARCHAR(100),(tbM1.Eql))) + '; '
+		FROM #tbMais2008 tbM1 WITH(NOLOCK)
+		WHERE  tbM.Num_CliCPFCNPJ = tbM1.Num_CliCPFCNPJ
+		FOR XML PATH('')
+	)mo (mostra)
+	WHERE tbM.numRow = 1
+
+SELECT	ROW_NUMBER() OVER(PARTITION BY cl.Num_CliCPFCNPJ ORDER BY cl.Num_CliCPFCNPJ) AS numRow,
+		(CONVERT(VARCHAR(50), lt.Num_Empr) + ' - ' + lt.Cod_Quadra + ' - ' + CONVERT(VARCHAR(50), lt.Cod_Lot)) AS Eql,
+		lt.Cod_Lot,
+		cl.Num_CliCPFCNPJ,
+		cl.Nom_Cli,
+		ct.Dat_Venda,
+		ct.Dat_ConfirmaVenda,
+		sv.Nom_StatusContVend
+	INTO #tbTudo
+	FROM GKSLT_Contratos ct WITH(NOLOCK)
+	INNER JOIN GKSLT_Clientes cl WITH(NOLOCK)
+		ON ct.Num_CliCPFCNPJ = cl.Num_CliCPFCNPJ
+	INNER JOIN GKSLT_Lotes lt WITH(NOLOCK)
+		ON ct.Num_ChavLot = lt.Num_ChavLot
+	INNER JOIN GKSLT_StatusContVend  sv
+		ON ct.Cod_StatusContVend = sv.Cod_StatusContVend
+	WHERE sv.Cod_StatusContVend = 2 AND YEAR(ct.Dat_Venda) < 2008
+	ORDER BY ct.Dat_Venda DESC
+
+SELECT tb.Num_CliCPFCNPJ,
+	SUBSTRING(mostra,1,LEN(mostra) - 1) AS junta
+	INTO #tbTempJunta
+	FROM #tbTudo tb WITH(NOLOCK)
+	CROSS APPLY(SELECT (CONVERT(VARCHAR(100),(tb1.Cod_Lot))) + ', '
+		FROM #tbTudo tb1 WITH(NOLOCK)
+		WHERE  tb.Num_CliCPFCNPJ = tb1.Num_CliCPFCNPJ
+		FOR XML PATH('')
+	)mo (mostra)
+	WHERE tb.numRow = 1
+
+SELECT	tb.EqlJunto AS eql,
+		tb.Num_CliCPFCNPJ,
+		tb.Nom_Cli,
+		tb.Dat_Venda,
+		tb.Dat_ConfirmaVenda,
+		tb.Nom_StatusContVend,
+		tj.junta AS LotesAntigos
+	FROM #tbTempJunta2008 tb WITH(NOLOCK)
+	LEFT JOIN #tbTempJunta tj WITH(NOLOCK)
+		ON tb.Num_CliCPFCNPJ = tj.Num_CliCPFCNPJ
+
+
+	drop table #tbMais2008
+	drop table #tbTempJunta2008
+	drop table #tbTempJunta
+	drop table #tbTudo
+	
+ 
---
+++ Exercicio BD - EstagioM/Script_Ex05.sql
@@ -0,0 +1,5 @@
+/*
+	[10/10/2018] Eduardo: Levantamento das vendas a partir de 01/2008 com a 1ª parcela de terra paga.
+	Todas as vendas a partir de 01/2018. Onde a 1ª parcela de terra (Cod_TipParc = 3) está quitada, 
+	mas possui alguma parcela de sinal ou corretagem (97 e 99) em aberto.
+*/
